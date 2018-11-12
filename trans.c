a31f074e1b3096d45d4e1778b7a762ce3c4ac79b
qogusdn <qogusdn777@naver.com>
2018-11-12 22:08:54 +0900

    2

--- trans.c
+++ trans.c
@@ -14,13 +14,24 @@ int instr_trans(char *op, char *args, char* mcode)
 	
 	while(args[i]!=NULL){
 		if(args[i]==','){
-			if((args[i+1]=='%')==0 && (args[0]=='%') )
+			if((args[i+1]=='%')==0 && (args[0]=='%') )//1
 				mcode="89";
-			else if(args[i+1]=='%' && (args[0]=='(' && args[i-1]==')'))
+			else if(args[i+1]=='%' && (args[i-1]==')'))//2,3
 				mcode="8b";
-               		else if((args[i+1]=='%') && (args[0]=='$'))
-				mcode="b8";
-			else if(args[i+1]=='
+			else if(args[i+1]=='%' && args[0]=='0')//4
+				mcode="a1";
+			else if(args[i+1]=='0' && args[0]=='%')//5
+				mcode="a3";
+               		else if((args[i+1]=='%') && (args[0]=='$')){  //6
+				if(args[i+3]=='a')
+					mcode="b8";
+				else if(args[i+3]=='c')
+					mcode="b9";
+				else if(args[i+3]=='d')
+					mcode="ba";
+				else if(args[i+3]=='b')
+					mcode="bb";
+			}
 		}
 	
 	i++;
