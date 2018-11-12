43e41179e0a2f2c1e9c55b1c67074a0783c8673e
qogusdn <qogusdn777@naver.com>
2018-11-12 22:26:34 +0900

    3

--- check.c
+++ check.c
@@ -2,10 +2,23 @@
 
 int is_valid(char *op, char *args)
 {
+	int i=0;
 	//printf("if valid, return 1\n");
 	//printf("otherwise, return 0\n");
-
-	if(
+	
+	while(args[i]!=NULL){
+		if(arg[i]==','){
+			if(args[i+1]=='$')
+				return 0;
+			else if(args[i+1]=='0')
+				if(args[0]=='0' || args[0]=='(')
+				return 0;
+			else if(args[i+1]=='(')
+				if(args[0]=='(' || args[0]=='0')
+				return 0;
+	}
+	i++;
+	}
 	
 	return 1;
 }
